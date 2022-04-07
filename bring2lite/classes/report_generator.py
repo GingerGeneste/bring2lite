0dc5423634081ae0f8e5dd9a0429d839163ad83f
Ginger Geneste <g.geneste@nfi.minvenj.nl>
2022-04-07 16:03:17 +0200

    Added first attempt to generate a log through CSV

--- bring2lite/classes/report_generator.py
+++ bring2lite/classes/report_generator.py
@@ -7,6 +7,54 @@ class ReportGenerator:
     def __init__(self):
         self.my_path = ""
 
+    def generateCSV(self, path, filename, data, schema=["No schema found"]):
+        """
+        Alternate results writer, using a CSV module to avoid not escaping comma's e.d.
+        :param path: path to directory to write the log files to
+        :param filename: name of the logfile (without .log postfix)
+        :param data:
+        :param schema:
+        :return:
+        """
+        if data is None:
+            return
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        # Iterator to create CSV from
+        out = []
+        # If a schema is found, add the column types to the top of the log file:
+        out.append(schema)
+        # A 'frame' is actually a possible carved record
+        # Represented as a list where each element is a list containing the type of
+        # column value and the column value itself
+        for frame in data:
+            csv_row = []
+            if isinstance(frame, list):
+                for column in frame:
+                    # If the carved record column is of type 'text' try to decode it
+                    if column[0] == 'TEXT':
+                        try:
+                            csv_row.append(column[1].decode('utf-8'))
+                        except UnicodeDecodeError:
+                            csv_row.append(column[1])
+                    # Otherwise just add the value as it is:
+                    else:
+                        csv_row.append(column[1])
+            out.append(csv_row)
+
+        # Write results
+        try:
+            with open(f'{path}/test_{filename}.log', 'w', newline='') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerows(out)
+
+        except UnicodeEncodeError:
+            tqdm.write("can not write the record because of unicode errors")
+        return out
+
+
     def generateReport(self, path, filename, data, format="CSV", schema=["No schema found"]):
         if data is None:
             return
@@ -18,6 +66,7 @@ class ReportGenerator:
         for datatype in schema:
             out += str(datatype) + ","
         out += "\n"
+        test = self.generateCSV(path, filename, data, schema)
 
         for frame in data:
             if isinstance(frame, list):
